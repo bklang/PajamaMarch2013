ad7b7f0587468106ac532e3eba26f993d640b3c5
Ben Klang <bklang@mojolingo.com>
2013-05-25 14:25:40 -0400

    Add switch to cycle modes
    Also add new hard_random animation and reduce light switch count to 7

--- PJ13_Arduino/PJ13_Arduino.ino
+++ PJ13_Arduino/PJ13_Arduino.ino
@@ -1,4 +1,9 @@
 #define MAX_DUTY_CYCLE 32
+#define TOP_PIN 6
+#define SWITCH_PIN 8
+// Debounce in microseconds
+#define DEBOUNCE_DELAY 50000
+#define MODE_COUNT 6
 
 const int slow_delay = 25;
 const int med_delay = 15;
@@ -18,13 +23,17 @@ int lowerPin = 5;
 int lastAnimation = 0;
 int lastDelay = 0;
 int currentDirection = 0;
+int switchCount = 0;
+int switchPosition = 0;
 
 void setup()
 {
   int i;
-  for (i = 0; i <= 13; i++) {
+  for (i = 0; i <= TOP_PIN; i++) {
     pinMode(i, OUTPUT);
   } 
+  pinMode(SWITCH_PIN, INPUT);
+  switchPosition = digitalRead(SWITCH_PIN);
 }
 
 void loop()
@@ -33,17 +42,49 @@ void loop()
   
   lastDelay++;
   if (lastDelay > no_delay) {
-    //shimmer(med_fade);
-    //light_random(fast_fade);
-    //fade_run(fast_fade);
-    fade_split(fast_fade);
-    //fade_random(fast_fade);
-
+    // Reset delay counter
     lastDelay = 0;
+
+    // Check if switch has moved
+    if (digitalRead(SWITCH_PIN) != switchPosition) {
+      delayMicroseconds(DEBOUNCE_DELAY);
+
+      // read again to ensure we are not bouncing
+      if (digitalRead(SWITCH_PIN) != switchPosition) {
+        switchPosition = digitalRead(SWITCH_PIN);
+        switchCount++;
+        if (switchCount >= MODE_COUNT) {
+          switchCount = 0;
+        }
+      }
+    }
+
+    // Run the current animation
+    switch(switchCount) {
+    case 0:
+      shimmer(med_fade);
+      break;
+    case 1:
+      light_random(fast_fade);
+      break;
+    case 2:
+      hard_random(fast_fade);
+      break;
+    case 3:
+      fade_run(slow_fade);
+      break;
+    case 4:
+      fade_split(med_fade);
+      break;
+    case 5:
+      fade_random(fast_fade);
+      break;
+    }
   }
 
+  // Pulse the LEDs
   for (i = 0; i <= 255; i++) {
-    for (pin = 0; pin <= 13; pin++) {
+    for (pin = 0; pin <= TOP_PIN; pin++) {
       if (pinValues[pin] > i && i < MAX_DUTY_CYCLE) {
         lighton(pin);
       } else {
@@ -55,7 +96,7 @@ void loop()
 
 void shimmer(int delay) {
   int pin = 0;
-  for (pin = currentPin++; pin <= 13; pin++) {
+  for (pin = currentPin++; pin <= TOP_PIN; pin++) {
     if (pinDirections[pin] == 0) {
       fade_in(pin, delay);
       if (pinValues[pin] == MAX_DUTY_CYCLE) { pinDirections[pin] = 1; }
@@ -64,7 +105,7 @@ void shimmer(int delay) {
       if (pinValues[pin] == 0) { pinDirections[pin] = 0; }
     }
   }
-  if (currentPin > 13) { currentPin = 0; }
+  if (currentPin > TOP_PIN) { currentPin = 0; }
 }
 
 void light_random(int delay)
@@ -77,13 +118,28 @@ void light_random(int delay)
   }
 }
 
+void hard_random(int delay)
+{
+  int pin = 0;
+  lastAnimation++;
+  if (lastAnimation >= delay) {
+    lastAnimation = 0;
+    pin = randomPin();
+    if (pinValues[pin] == 0) {
+      pinValues[pin] = MAX_DUTY_CYCLE;
+    } else {
+      pinValues[pin] = 0;
+    }
+  }
+}
+
 void fade_run(int delay) {
   if (currentDirection == 0) {
     fade_in(currentPin, delay);
     if (pinValues[currentPin] == MAX_DUTY_CYCLE) {
       currentPin++;
     }
-    if (currentPin > 13) {
+    if (currentPin > TOP_PIN) {
       currentPin = 0;
       currentDirection = 1;
     }
@@ -92,7 +148,7 @@ void fade_run(int delay) {
     if (pinValues[currentPin] == 0) {
       currentPin++;
     }
-    if (currentPin > 13) {
+    if (currentPin > TOP_PIN) {
       currentPin = 0;
       currentDirection = 0;
     }
@@ -107,9 +163,9 @@ void fade_split(int delay) {
       upperPin++;
       lowerPin--;
     }
-    if (upperPin > 13) {
-      upperPin = 6;
-      lowerPin = 5;
+    if (upperPin > TOP_PIN) {
+      upperPin = TOP_PIN/2;
+      lowerPin = TOP_PIN/2 - 1;
       currentDirection = 1;
     }
   } else {
@@ -119,9 +175,9 @@ void fade_split(int delay) {
       upperPin++;
       lowerPin--;
     }
-    if (upperPin > 13) {
-      upperPin = 6;
-      lowerPin = 5;
+    if (upperPin > TOP_PIN) {
+      upperPin = TOP_PIN/2;
+      lowerPin = TOP_PIN/2 - 1;
       currentDirection = 0;
     }
   }
@@ -145,7 +201,7 @@ void fade_random(int delay) {
 
 int randomPin()
 {
-  return random(0, 14);
+  return random(0, TOP_PIN + 1);
 }
 
 void lighton(int pin)
